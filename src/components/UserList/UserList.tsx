076609bebe41b947ce3531bdf92ebe8297038d35
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-08-19 18:01:58 +0200

    Integrate RepositoryList into UserList and pass repos as props

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'; 
 import { User } from '../../types/user';
+import RepositoryList from '../RepositoryList/RepositoryList';
 
 interface UserListProps {
   users: User[];
   onUserClick: (user: User) => void;
   expandedUsers: { [key: number]: boolean };
+  selectedUserRepos: { [key: number]: any[] };
 }
 
-const UserList: React.FC<UserListProps> = ({ users, onUserClick, expandedUsers }) => {
+const UserList: React.FC<UserListProps> = ({
+  users,
+  onUserClick,
+  expandedUsers,
+  selectedUserRepos,
+}) => {
   return (
     <div className="user-list">
       {users.map((user) => {
         const isExpanded = expandedUsers[user.id];
+        const userRepos = selectedUserRepos[user.id] || [];
 
         return (
           <React.Fragment key={user.id}>
@@ -25,6 +33,12 @@ const UserList: React.FC<UserListProps> = ({ users, onUserClick, expandedUsers }
                 </span>
               </div>
             </div>
+
+            {isExpanded && (
+              <div className="user-repos">
+                <RepositoryList repos={userRepos} />
+              </div>
+            )}
           </React.Fragment>
         );
       })}
