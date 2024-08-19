020f55861c45a8470922cf3a28bd26123bfbd407
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-08-19 18:17:15 +0200

    Add logic to fetch repositories for users in SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from '../../redux/store';
+import { getUserRepos } from '../../api/githubApi';
 import UserList from '../UserList/UserList';
 import { User } from '../../types/user';
 
 const SearchBar: React.FC = () => {
+  const dispatch = useDispatch();
   const users = useSelector((state: AppState) => state.users);
   const [expandedUsers, setExpandedUsers] = useState<{ [key: number]: boolean }>({});
+  const [selectedUserRepos, setSelectedUserRepos] = useState<{ [key: number]: any[] }>({});
 
-  const handleUserClick = (user: User) => {
+  const handleUserClick = async (user: User) => {
     setExpandedUsers(prev => ({ ...prev, [user.id]: !prev[user.id] }));
+
+    if (!selectedUserRepos[user.id]) {
+      const { repos } = await getUserRepos(user.login);
+      setSelectedUserRepos(prev => ({ ...prev, [user.id]: repos }));
+    }
   };
 
   return (
@@ -18,6 +26,7 @@ const SearchBar: React.FC = () => {
         users={users} 
         onUserClick={handleUserClick} 
         expandedUsers={expandedUsers} 
+        selectedUserRepos={selectedUserRepos}
       />
     </div>
   );
