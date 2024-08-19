551bc2f33e215614a38d8226e5dd851181eba244
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-08-19 18:39:15 +0200

    Display total repository count for each user in UserList

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -9,6 +9,7 @@ interface UserListProps {
   onUserClick: (user: User) => void;
   expandedUsers: { [key: number]: boolean };
   selectedUserRepos: { [key: number]: any[] };
+  totalReposCount: { [key: number]: number };
 }
 
 const UserList: React.FC<UserListProps> = ({
@@ -16,18 +17,21 @@ const UserList: React.FC<UserListProps> = ({
   onUserClick,
   expandedUsers,
   selectedUserRepos,
+  totalReposCount,
 }) => {
   return (
     <div className="user-list">
       {users.map((user) => {
         const isExpanded = expandedUsers[user.id];
         const userRepos = selectedUserRepos[user.id] || [];
+        const totalCount = totalReposCount[user.id] || 0;
 
         return (
           <React.Fragment key={user.id}>
             <div className="user-item" onClick={() => onUserClick(user)}>
               <div className="user-header">
                 <h3>{user.login}</h3>
+                <span className="repo-count">Repos: {totalCount}</span>
                 <span className="dropdown-icon">
                   <FontAwesomeIcon icon={isExpanded ? faChevronUp : faChevronDown} />
                 </span>
